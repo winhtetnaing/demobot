321d8626269640177bdde7d99c7fb2e4ee0c1ee2
win htet naing <37439378+winhtetnaing@users.noreply.github.com>
2018-09-11 10:21:19 +0630

    add

---
+++ helpers/processMessage.js
@@ -0,0 +1,34 @@
+//‘your Dialogflow Client Access Token’
+const API_AI_TOKEN = ;
+const apiAiClient = require(‘apiai’)(API_AI_TOKEN);
+//‘your Facebook Page Access Token’
+const FACEBOOK_ACCESS_TOKEN = 'EAAEX8u1ysWMBAEPrq8JqKjtKeUlQohxKQyrRt0l5OnxWOKJSr6SPCNo0r7FU1fxeSjTsvS9aTHWFeGn78jD1XUaP8qNPXV8tpe8VakSo5YjQrcfwgaAZCyiovLoRxayWokciUdjMRly2awH8TR8m2Gagm5ZBFCtjTLSwW8uwZDZD';
+const request = require(‘request’);
+
+const sendTextMessage = (senderId, text) => {
+ request({
+ url: ‘https://graph.facebook.com/v2.6/me/messages',
+ qs: { access_token: FACEBOOK_ACCESS_TOKEN },
+ method: ‘POST’,
+ json: {
+ recipient: { id: senderId },
+ message: { text },
+ }
+ });
+};
+
+module.exports = (event) => {
+ const senderId = event.sender.id;
+ const message = event.message.text;
+
+const apiaiSession = apiAiClient.textRequest(message, {sessionId: ‘crowdbotics_bot’});
+
+apiaiSession.on(‘response’, (response) => {
+ const result = response.result.fulfillment.speech;
+
+sendTextMessage(senderId, result);
+ });
+
+apiaiSession.on(‘error’, error => console.log(error));
+ apiaiSession.end();
+};
