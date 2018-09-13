d5ecd2ef9b4f565966f85544d0016797ea0122e5
win htet naing <37439378+winhtetnaing@users.noreply.github.com>
2018-09-13 11:44:55 +0630

    add

--- controllers/messageWebhook.js
+++ controllers/messageWebhook.js
@@ -1,8 +1,8 @@
 const processMessage = require('../helpers/processMessage');
 module.exports = (req, res) => {
- console.log("req data: "+req);
+ //console.log("req data: "+req);
  if (req.body.object === 'page') {
-  console.log("enter if cond: "+req);
+  //console.log("enter if cond: "+req);
  req.body.entry.forEach(entry => {
  entry.messaging.forEach(event => {
  if (event.message && event.message.text) {
